5b7d92c52b473c78e7b8885c1b0de1be6a5c18a4
Vlad <48223023+Edjll@users.noreply.github.com>
2019-03-23 16:19:22 +0400

    Add files via upload

---
+++ js/flexDirection.js
@@ -0,0 +1,7 @@
+scanContainer(4);
+arr(4);
+
+game (firstFlexAttribute, ' flexDirection', 'firstFlexContainer', 'column', 0, 4, namesFlexDirection, 2, lengtFlexDirection);
+game (secondFlexAttribute, ' flexDirection', 'secondFlexContainer', 'row-reverse', 1, 4, namesFlexDirection, 2, lengtFlexDirection);
+game (thirdFlexAttribute, ' flexDirection', 'thirdFlexContainer', 'column-reverse', 2, 4, namesFlexDirection, 2, lengtFlexDirection);
+game (fourthFlexAttribute, ' flexDirection', 'fourthFlexContainer', 'row', 3, 4, namesFlexDirection, 2, lengtFlexDirection);
--- js/gameScript.js
+++ js/gameScript.js
@@ -1,18 +1,26 @@
 var amountTrueAnswer = 0;
 var question = [];
-var flexContainerDefault = document.getElementById('firstFlexContainer').className;
+var flexContainer = [];
+var defaulFlexGame = 'flexBlock';
 var defaulInputClass = document.getElementById('firstFlexAttribute').className;
-var names = ['Row', 'Column', 'Reverse'];
+var defaultFlexElements = 'flexElement';
+var namesContainers = ['firstFlexContainer', 'secondFlexContainer', 'thirdFlexContainer', 'fourthFlexContainer'];
+var namesFlexDirection = ['Row', 'Column', 'Reverse'];
+var namesFlexWrap = ['Nowrap', 'Wrap', 'Reverse'];
 var lengtFlexDirection = [3, 6, 11, 14];
+var lengtFlexWrap = [6, 4, 12];
 
+function scanContainer(amountQuestion) {
+	for (var i = 0; i < amountQuestion; i++) {
+		flexContainer[i] = document.getElementById(namesContainers[i]).className;
+	}
+}
 
 function arr(amountQuestion) {
 	for (var i = 0; i < amountQuestion; i++) {
 		question[i] = 0;
 	}
 }
-
-arr(4);
 
 function sumAnswer(amountQuestion) {
 	amountTrueAnswer = 0;
@@ -21,38 +29,43 @@ function sumAnswer(amountQuestion) {
 	}
 }
 
-function game(id, type, idBlock, name, number, amountQuestion) {
+function colorElements(elements, color) {
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].className = defaultFlexElements + color;
+	}
+}
+
+function game(id, type, idBlock, name, number, amountQuestion, names, limit, length) {
 	id.oninput = function() {
 		var inputElement = document.getElementById(this.id);
 		var classElement = document.getElementById(idBlock);
+		var elementsColor = classElement.getElementsByTagName('*');
 		var att;
 		question[number] = 0;
 		inputElement.className = defaulInputClass;
-		for (var i = 0; i < 2; i++) { 
+		classElement.className = flexContainer[number];
+		colorElements(elementsColor, '');
+		for (var i = 0; i < limit; i++) { 
 			if (inputElement.value === names[i].toLowerCase()) {
-				classElement.className = flexContainerDefault + type + names[i];
+				classElement.className = defaulFlexGame + type + names[i];
 				inputElement.className = defaulInputClass + ' falseAnswer';
 			}
-			if (inputElement.value === (names[i].toLowerCase() + '-' + names[2].toLowerCase())) {
-				classElement.className = flexContainerDefault + type + names[i] + names[4];
+			if (inputElement.value === (names[i].toLowerCase() + '-' + names[limit].toLowerCase())) {
+				classElement.className = defaulFlexGame + type + names[i] + names[limit];
 				inputElement.className = defaulInputClass + ' falseAnswer';
 			}
 		}
 		if (inputElement.value === name) {
 			for (var i = 0; i < amountQuestion; i++) {
-				if (name.length === lengtFlexDirection[i]) att = names[i];
-				if (name.length === lengtFlexDirection[i] && name.length > 6) att = names[i-2] + names[2];
+				if (name.length === length[i]) att = names[i];
+				if (name.length === length[i] && name.length > 6) att = names[i-limit] + names[limit];
 			}
-			classElement.className = flexContainerDefault + type + att;
+			classElement.className = defaulFlexGame + type + att;
 			inputElement.className = defaulInputClass + ' trueAnswer';
+			colorElements(elementsColor, ' flexElementTrue');
 			question[number] = 1;
 		}
 		sumAnswer(amountQuestion);
 		document.getElementById('result').innerHTML = 'Ваш результат: ' + amountTrueAnswer + ' из ' + amountQuestion +'.';
 	} 
 }
-
-game (firstFlexAttribute, ' flexDirection', 'firstFlexContainer', 'column', 0, 4);
-game (secondFlexAttribute, ' flexDirection', 'secondFlexContainer', 'row-reverse', 1, 4);
-game (thirdFlexAttribute, ' flexDirection', 'thirdFlexContainer', 'column-reverse', 2, 4);
-game (fourthFlexAttribute, ' flexDirection', 'fourthFlexContainer', 'row', 3, 4);
