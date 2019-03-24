b146775b54d4fd947c6192c1d149a294ae817156
Vlad <48223023+Edjll@users.noreply.github.com>
2019-03-24 14:52:33 +0400

    Add files via upload

--- js/flexWrap.js
+++ js/flexWrap.js
@@ -1,6 +1,6 @@
 scanContainer(3);
 arr(3);
 
-game (firstFlexAttribute, ' flexWrap', 'firstFlexContainer', 'nowrap', 0, 3, namesFlexWrap, 2, lengtFlexWrap);
-game (secondFlexAttribute, ' flexWrap', 'secondFlexContainer', 'wrap', 1, 3, namesFlexWrap, 2, lengtFlexWrap);
-game (thirdFlexAttribute, ' flexWrap', 'thirdFlexContainer', 'wrap-reverse', 2, 3, namesFlexWrap, 2, lengtFlexWrap);
+game (firstFlexAttribute, ' flexWrap', 'firstFlexContainer', 'nowrap', 0, 3, namesFlexWrap, 'flexBlock');
+game (secondFlexAttribute, ' flexWrap', 'secondFlexContainer', 'wrap', 1, 3, namesFlexWrap, 'flexBlock');
+game (thirdFlexAttribute, ' flexWrap', 'thirdFlexContainer', 'wrap-reverse', 2, 3, namesFlexWrap, 'flexBlock');
--- js/gameScript.js
+++ js/gameScript.js
@@ -1,14 +1,18 @@
 var amountTrueAnswer = 0;
 var question = [];
 var flexContainer = [];
-var defaulFlexGame = 'flexBlock';
+
 var defaulInputClass = document.getElementById('firstFlexAttribute').className;
 var defaultFlexElements = 'flexElement';
-var namesContainers = ['firstFlexContainer', 'secondFlexContainer', 'thirdFlexContainer', 'fourthFlexContainer'];
-var namesFlexDirection = ['Row', 'Column', 'Reverse'];
-var namesFlexWrap = ['Nowrap', 'Wrap', 'Reverse'];
-var lengtFlexDirection = [3, 6, 11, 14];
-var lengtFlexWrap = [6, 4, 12];
+
+var namesContainers = ['firstFlexContainer', 'secondFlexContainer', 'thirdFlexContainer', 'fourthFlexContainer', 'fivethFlexContainer', 'sixthFlexContainer', 'seventhFlexContainer'];
+var namesFlexDirection = ['Row', 'Column', 'RowReverse', 'ColumnReverse'];
+var namesFlexWrap = ['Wrap', 'Nowrap', 'WrapReverse'];
+var namesFlexFlow = ['ColumnWrap', 'RowWrapReverse', 'RowReverseNowrap', 'ColumnWrapReverse', 'ColumnNowrap', 'RowWrap', 'RowNowrap', 'RowReverseWrap', 'RowReverseWrapReverse', 'ColumnReverseWrap', 'ColumnReverseWrapReverse', 'ColumnReverseNowrap'];
+var namesFlexJustifyContent = ['SpaceBetween', 'FlexEnd', 'Center', 'FlexStart', 'SpaceEvenly', 'SpaceAround'];
+var namesFlexAlignItems = ['Center', 'FlexStart', 'FlexEnd', 'Baseline', 'Stretch'];
+var namesFlexAlignContent = ['FlexEnd', 'FlexStart', 'SpaceAround', 'Center', 'Stretch', 'SpaceBetween', 'SpaceEvenly'];
+
 
 function scanContainer(amountQuestion) {
 	for (var i = 0; i < amountQuestion; i++) {
@@ -35,7 +39,13 @@ function colorElements(elements, color) {
 	}
 }
 
-function game(id, type, idBlock, name, number, amountQuestion, names, limit, length) {
+function newName(name) {
+	name = name.replace(' ', '');
+	name = name.replace('-', '');
+	return name;
+}
+
+function game(id, type, idBlock, name, number, amountQuestion, names, defaulFlexGame) {
 	id.oninput = function() {
 		var inputElement = document.getElementById(this.id);
 		var classElement = document.getElementById(idBlock);
@@ -44,23 +54,15 @@ function game(id, type, idBlock, name, number, amountQuestion, names, limit, len
 		question[number] = 0;
 		inputElement.className = defaulInputClass;
 		classElement.className = flexContainer[number];
-		colorElements(elementsColor, '');
-		for (var i = 0; i < limit; i++) { 
-			if (inputElement.value === names[i].toLowerCase()) {
+		colorElements(elementsColor, ' flexElementFalse');
+		for (var i = 0; i < names.length; i++) { 
+			var inputNewName = newName(inputElement.value);
+			if (inputNewName === names[i].toLowerCase()) {
 				classElement.className = defaulFlexGame + type + names[i];
-				inputElement.className = defaulInputClass + ' falseAnswer';
-			}
-			if (inputElement.value === (names[i].toLowerCase() + '-' + names[limit].toLowerCase())) {
-				classElement.className = defaulFlexGame + type + names[i] + names[limit];
 				inputElement.className = defaulInputClass + ' falseAnswer';
 			}
 		}
 		if (inputElement.value === name) {
-			for (var i = 0; i < amountQuestion; i++) {
-				if (name.length === length[i]) att = names[i];
-				if (name.length === length[i] && name.length > 6) att = names[i-limit] + names[limit];
-			}
-			classElement.className = defaulFlexGame + type + att;
 			inputElement.className = defaulInputClass + ' trueAnswer';
 			colorElements(elementsColor, ' flexElementTrue');
 			question[number] = 1;
